4b5e3fdf4dfc7d5db77f8dd60b74c6c598337dd5
Marie Macapagal <emariemacapagal@gmail.com>
2024-02-25 20:06:06 +0800

    update

---
+++ public/assets/js/config.js
@@ -0,0 +1,1 @@
+"use strict";let config={colors:{primary:"#4170d4",secondary:"#6a99f0",success:"#28a745",info:"#03c3ec",warning:"#ffab00",danger:"#dc3545",dark:"#233446",black:"#000",white:"#fff",cardColor:"#fff",bodyBg:"#f5f5f9",bodyColor:"#697a8d",headingColor:"#566a7f",textMuted:"#a1acb8",borderColor:"#eceef1"}};
