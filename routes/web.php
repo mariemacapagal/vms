265bd7bac8a08af5c40fda46d4c2172f416655db
Marie Macapagal <emariemacapagal@gmail.com>
2024-04-07 20:17:15 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::middleware(['auth', 'user-access:Admin'])->group(function () {
 
   // Route for Blocked Visitors
   Route::get('/records/blocked-visitors', VisitorController::class . '@blockedList')->name('visitors.blocked');
-  Route::get('/records/blocked-visitors/history', VisitorController::class . '@blockedListHistory')->name('visitors.blocked');
+  Route::get('/records/blocked-visitors/history', VisitorController::class . '@blockedListHistory')->name('visitors.blockedHistory');
   Route::match(['post', 'delete'], '/blocked-visitors/{visitor}', VisitorController::class . '@unblockVisitors')->name('visitors.unblock');
   Route::get('/blocked-visitors/export', VisitorController::class . '@blockedVisitorsExport')->name('blockedvisitors.export');
 });
